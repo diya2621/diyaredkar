9cc75ea8df3e54fdcc784dee17410330ac97a456
diya2621 <diyaredkar2602@gmail.com>
2024-12-24 14:00:13 +0530

    Add files via upload

---
+++ p.js
@@ -0,0 +1,7 @@
+const items=document.querySelectorAll(".add-to-cart");
+items.forEach(button =>{
+    button.addEventListener("click",() =>{
+        alert('Your items are added to cart');
+
+    });
+});
